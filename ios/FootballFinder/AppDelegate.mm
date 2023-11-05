d400537a3b7fe06ba0d1d5e80343e3f6707107f2
Szczepan Piwowarski <78231699+pivvovarsky@users.noreply.github.com>
2023-11-05 11:53:11 +0100

    rename to Football Finder

--- ios/FootballFinder/AppDelegate.mm
+++ ios/FootballFinder/AppDelegate.mm
@@ -6,7 +6,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"FootballFinder";
+  self.moduleName = @"Football Finder";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
